cdccaa28821e64c49df4fc26a91116a0da214489
Eryk <you@example.com>
2020-04-21 20:11:48 +0200

    Remove import

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux';
 import loginReducer from '../pages/login/reducers/loginReducer';
 import registerReducer from '../pages/register/reducers/registerReducer';
-import getCartSlice from '../pages/cart/reducer/getCartSlice';
-import addCartSlice from '../pages/cart/reducer/addToCartSlice';
-import
 
 export const allReducers = combineReducers({
   login: loginReducer,
