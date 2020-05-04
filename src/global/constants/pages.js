f21c22f452a2b0032a6c9def85e96456870883e6
Dominik Kaczmarczyk <dominik.kaczmarczyk961@gmail.com>
2020-05-04 17:34:27 +0200

    Added navbar Search form

--- src/global/constants/pages.js
+++ src/global/constants/pages.js
@@ -3,3 +3,4 @@ export const LOGIN_PAGE = '/login';
 export const REGISTER_PAGE = '/register';
 export const MAIN_PAGE = '/';
 export const CATEGORY_PAGE = '/category';
+export const SEARCH_PAGE = './search';
