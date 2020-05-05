ad85612334b74c008d4492d0cb14fb4878006777
Yevhenii Natalenko <zeni@zeniMB.lan>
2020-05-05 16:47:04 +0200

    refactor

--- src/global/mock/book.js
+++ src/global/mock/book.js
@@ -6,9 +6,9 @@ export const response = {
       "id": 0,
       "name": "novel"
     },
-    "year": 0,
+    "year": 1890,
     "photoUrl": "https://www.wprost.pl/_thumb/ed/44/bf498afd9f658432481a79bd8d0c.png",
     "description": "opis fabuły",
     "available": true,
-    "price": 0
+    "price": 39.90
 };
