4983e48b028e0c816f7cc0110b8db76555f62896
Dominik Kaczmarczyk <dominik.kaczmarczyk961@gmail.com>
2020-06-08 11:25:10 +0200

    Moved sidebar to components and made it more versatile. Added sidebar to admin panel

--- src/pages/admin-panel/AdminPanel.js
+++ src/pages/admin-panel/AdminPanel.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { sidebar } from 'global/constants/adminPanel';
+import {
+  ADMIN_PAGE,
+  ADMIN_PAGE_ORDERS,
+  ADMIN_PAGE_USERS,
+  ADMIN_PAGE_BOOKS,
+} from 'global/constants/pages';
+import { Box } from '@material-ui/core';
+
+import PrivateRoute from 'components/privateRoute/PrivateRoute';
+import Sidebar from 'components/sidebar/Sidebar';
+import Books from './components/books/Books';
+import Orders from './components/orders/Orders';
+import Users from './components/users/Users';
+
+import useAdminPanelStyles from './AdminPanelStyles';
 
 const AdminPanel = () => {
-  return <div>AdminPanel</div>;
+  const classes = useAdminPanelStyles();
+
+  return (
+    <Box className={classes.container}>
+      <Sidebar
+        title={sidebar.title}
+        items={sidebar.items}
+        baseItemUrl={ADMIN_PAGE}
+      />
+      <Switch>
+        <Route
+          exact
+          path={[ADMIN_PAGE, ADMIN_PAGE_ORDERS]}
+          component={Orders}
+        />
+        <Route exact path={ADMIN_PAGE_USERS} component={Users} />
+        <Route exact path={ADMIN_PAGE_BOOKS} component={Books} />
+      </Switch>
+    </Box>
+  );
 };
 
 export default AdminPanel;
---
+++ src/pages/admin-panel/components/books/Books.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Books = () => {
+  return <div>Admin Panel Books</div>;
+};
+
+export default Books;
