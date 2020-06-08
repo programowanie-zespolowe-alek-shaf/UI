2efda514894a779add59835b0a5d7afaf0d42422
Eryk <erykjarocki@gmail.com>
2020-06-08 08:51:31 +0200

    Separate customer details from login reducer (#24)

--- src/pages/customers/slice/customersSlice.js
+++ src/pages/customers/slice/customersSlice.js
@@ -1,10 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 import request from '../../../global/connection/backend/request';
 import { api } from '../../../global/connection/backend/endpoints';
+import { getCartFromStorage, getUsersCart } from '../../cart/slice/cartSlice';
+import { getUserDetails } from '../../login/actions/loginActions';
+
+const initialDetails = {
+  address: undefined,
+  email: undefined,
+  enabled: false,
+  firstName: undefined,
+  lastName: undefined,
+  lastShoppingCardId: null,
+  phone: undefined,
+  roles: [],
+  username: undefined,
+};
 
 export const initialState = {
   loading: false,
   error: undefined,
+  details: {
+    ...initialDetails
+  }
 };
 
 const customersSlice = createSlice({
@@ -16,19 +33,39 @@ const customersSlice = createSlice({
       state.error = undefined;
     },
     updateCustomerSuccess(state, action) {
-      state.loading = true;
+      state.loading = false;
+      state.details = action.payload;
     },
     updateCustomerError(state, action) {
-      state.loading = true;
+      state.loading = false;
       state.error = action.payload;
     },
+    requestCustomerDetails(state, action) {
+      state.loading = true;
+      state.error = undefined;
+    },
+    receiveCustomerDetails(state, action) {
+      state.loading = false;
+      state.details = action.payload;
+    },
+    receiveCustomerDetailsError(state,action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    clearCustomer(state,action) {
+      return initialState;
+    }
   },
 });
 const { actions, reducer } = customersSlice;
 export const {
   requestUpdateCustomer,
   updateCustomerError,
-  updateCustomerSuccess
+  updateCustomerSuccess,
+  requestCustomerDetails,
+  receiveCustomerDetails,
+  receiveCustomerDetailsError,
+  clearCustomer
 } = actions;
 
 export const updateCustomer = (body, userName) => (dispatch) => {
@@ -44,6 +81,25 @@ export const updateCustomer = (body, userName) => (dispatch) => {
   return update().then((response) => {
     dispatch(updateCustomerSuccess(response.data));
   }).catch((error) => { updateCustomerError(error.response && error.response.data.error); });
+};
+
+/** Fetches user details */
+export const getUserDetailsAction = (userName) => (dispatch) => {
+  const storageCartId = getCartFromStorage();
+  dispatch(requestCustomerDetails());
+
+  return getUserDetails(userName).then((response) => {
+    dispatch(receiveCustomerDetails(response.data));
+    dispatch(getUsersCart(storageCartId));
+
+    if (storageCartId !== response.data.id) {
+      const userDetailsWithUpdatedCartId = { ...response.data, lastShoppingCardId: storageCartId };
+      dispatch(updateCustomer(userDetailsWithUpdatedCartId, userName));
+    }
+
+  }).catch((error) => {
+    dispatch(receiveCustomerDetailsError(error.response && error.response.data.error));
+  });
 };
 
 export default reducer;
