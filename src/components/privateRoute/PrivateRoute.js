5015b1d96e30ffa809b9c64fe8a5cb0bd47aab8a
Eryk <erykjarocki@gmail.com>
2020-06-14 12:19:45 +0200

    Add Order page and modify redirect after login

--- src/components/privateRoute/PrivateRoute.js
+++ src/components/privateRoute/PrivateRoute.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { LOGIN_PAGE } from '../../global/constants/pages';
 import LoadingPage from '../../pages/loading/LoadingPage';
 import { shallowEqual, useSelector, useDispatch } from 'react-redux';
 import { triggerGlobalAlert } from 'components/globalAlert/slice/globalAlertSlice';
 import AdminRequired from '../../pages/admin-panel/pages/adminRequired/AdminRequired';
+import { setLastRoute } from '../../reducer/routeSlice';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
   const user = useSelector((state) => state.login, shallowEqual);
   const customer = useSelector((state) => state.customer, shallowEqual);
-  const dispatch = useDispatch();
 
   const { adminNeeded } = rest;
   const isAdmin = customer.details.isAdmin;
@@ -35,6 +39,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
           return <Component {...props} />;
         }
         if(user.isFetchingUser) return <LoadingPage />;
+        dispatch(setLastRoute(location.pathname));
         return <Redirect to={LOGIN_PAGE} />;
       }}
     />
