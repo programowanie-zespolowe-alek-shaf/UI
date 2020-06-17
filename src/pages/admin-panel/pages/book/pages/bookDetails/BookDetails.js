3b5959f12baabff98f7c12e865c909881fdee34e
Yevhenii Natalenko <work.natalenko@gmail.com>
2020-06-17 23:57:37 +0200

    admin book styling

--- src/pages/admin-panel/pages/book/pages/bookDetails/BookDetails.js
+++ src/pages/admin-panel/pages/book/pages/bookDetails/BookDetails.js
@@ -1,27 +1,84 @@
 import React from 'react';
 import useFeaturedStyles from "../../../../../main/components/featured/FeaturedStyles";
 import styles from "../../../../../book/styles/BookDetails.scss";
-import {Button} from "@material-ui/core";
+import {Box, Button, Card, CardActionArea, CardContent, CardMedia, Link, Typography} from "@material-ui/core";
 import {bool, PropTypes as pt} from "prop-types";
+import MainLayout from "../../../../../../components/mainLayout/MainLayout";
+import {BOOK_PAGE} from "../../../../../../global/constants/pages";
+import {Link as RouterLink} from "react-router-dom";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import useBookDetailsStyles from "../../../../../book/components/BookDetailsStyles";
+import {useDispatch} from "react-redux";
+import {addItemToCart} from "../../../../../cart/slice/cartSlice";
 
 const BookDetails = ({book}) => {
-  const classes = useFeaturedStyles();
+  const classes = useBookDetailsStyles();
+  const {
+    id,
+    title,
+    author,
+    description,
+    price,
+    year,
+    photoUrl,
+    available,
+    coverType,
+    numPages,
+  } = book;
+  const dispatch = useDispatch();
+
+  const InfoElement = ({ label, content }) => {
+    return (
+        <Box display='flex' flexDirection={'column'} mr={3}>
+          <Typography variant='caption' gutterBottom>
+            {label}
+          </Typography>
+          <Typography variant='subtitle2'>{content}</Typography>
+        </Box>
+    );
+  };
+
   return (
-      <div className={styles.container}>
-        <div className={styles.itemDetails}>
-          <img alt={book.imageURL} src={book.photoUrl} height={200} width={150}/>
-          <section className={styles.info}>
-            <header className={styles.title}>{book.title}</header>
-            <span>Loading: {book.loading}</span>
-            <span>Autor: {book.author}</span>
-            <span>Rok: {book.year}</span>
-            <span>Opis: {book.description}</span>
-            <span>Dostępna: {book.available}</span>
-            <span>Kategoria: {book.category.name}</span>
-            <span><strong>Cena: {book.price}</strong></span>
-          </section>
-        </div>
-      </div>
+        <Box display='flex' justifyContent='center' alignSelf='center' mt={1}>
+          <Card variant='outlined' classes={{ root: classes.root }}>
+            <CardActionArea classes={{ root: classes.actionArea }}>
+              <Link to={`${BOOK_PAGE}/${id}`} component={RouterLink}>
+                <CardMedia
+                    component='img'
+                    alt={title}
+                    image={photoUrl}
+                    title={title}
+                    className={classes.image}
+                />
+              </Link>
+            </CardActionArea>
+            <CardContent classes={{ root: classes.content }}>
+              <Typography variant='h6' className={classes.title}>
+                {title}
+              </Typography>
+
+              <Box display='flex' mt={1.5}>
+                <InfoElement label='Autor' content={author} />
+                <InfoElement label='Rok wydania' content={year} />
+              </Box>
+              <Box mt={1}>
+                <InfoElement label='Opis' content={description} />
+              </Box>
+              <Box display='flex' mt={1.5}>
+                <InfoElement label='Liczba stron' content={numPages} />
+                <InfoElement label='Typ okładki' content={coverType} />
+              </Box>
+              <Box mt={1.5}>
+                <InfoElement
+                    label='Cena'
+                    content={`${Number.parseFloat(price).toFixed(2)} zł`}
+                />
+              </Box>
+            </CardContent>
+          </Card>
+        </Box>
+
+
   );
 };
 
