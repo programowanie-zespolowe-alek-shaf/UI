e9a1a0b6fe5d2f0983271e723580b57ec452f852
Eryk <erykjarocki@gmail.com>
2020-06-18 08:28:38 +0200

    Add email to fixed values

--- src/pages/order/OrderContainer.js
+++ src/pages/order/OrderContainer.js
@@ -33,15 +33,15 @@ function OrderContainer() {
   return (
     <div className={styles.container}>
       {order.isSuccess ? <h2 className={styles.title}>ZAMÓWIENIE ZOSTAŁO ZŁOŻONE</h2> :
-          <React.Fragment>
-            <h2 className={styles.title}>PODSUMOWANIE</h2>
-            <div className={styles.content}>
-              <OrderData/>
-              <div className={styles.payment}>
-                <Payment/>
-              </div>
+        <React.Fragment>
+          <h2 className={styles.title}>PODSUMOWANIE</h2>
+          <div className={styles.content}>
+            <OrderData/>
+            <div className={styles.payment}>
+              <Payment/>
             </div>
-          </React.Fragment>
+          </div>
+        </React.Fragment>
       }
     </div>
   );
